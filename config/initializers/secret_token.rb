6a8ca3f2125a19ef7acb1b82bb27fcdd5e18ea8c
Eytan Anjel <anjel@usc.edu>
2013-06-24 01:23:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visualize::Application.config.secret_token = 'b63b9ce5374a63eb8c6a34d036c4e103779b6c4a94f5a5c3b45a48c97a295960a0fdb3b1b8722d098b3171ad8d8b53bc9fce2f57e32d412a8311a6a465044fa1'
